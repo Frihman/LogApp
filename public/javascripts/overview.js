f8068fec57c1e1ccb9f8c8742e2541c0c713e61c
James Frih <jamesfrih@me.com>
2021-01-02 18:12:19 +0100

    created dummydata for overview page

---
+++ public/javascripts/overview.js
@@ -0,0 +1,49 @@
+var logs = [{
+    "id" : 0,
+    "content" : "God Jul!",
+    "date" : "2020-12-24",
+    "favorite" : "true",
+    "mood" : "happy",
+    "authorId" : 0
+},
+{
+    "id" : 1,
+    "content" : "Merry Christmas!",
+    "date" : "2020-12-25",
+    "favorite" : "true",
+    "mood" : "happy",
+    "authorId" : 0  
+},
+{
+    "id" : 2,
+    "content" : "Happy Boxing Day!",
+    "date" : "2020-12-26",
+    "favorite" : "false",
+    "mood" : "happy",
+    "authorId" : 0  
+},
+{
+    "id" : 3,
+    "content" : "I very sad, no more chrimbus",
+    "date" : "2020-12-29",
+    "favorite" : "false",
+    "mood" : "sad",
+    "authorId" : 0  
+},
+{
+    "id" : 4,
+    "content" : "It's New Years Eve!",
+    "date" : "2020-12-31",
+    "favorite" : "false",
+    "mood" : "happy",
+    "authorId" : 0  
+},
+{
+    "id" : 5,
+    "content" : "Goodbye 2020!",
+    "date" : "2021-01-01",
+    "favorite" : "true",
+    "mood" : "happy",
+    "authorId" : 0  
+},
+]
